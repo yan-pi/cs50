ee15d6d0eba6c87b578720d5904a04a6fed56eb9
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-04-12 03:16:45 +0000

    add agree if conditional test

---
+++ module-01-c/06-agree/main.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    char c = get_char("Do you agree? ");
+
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Agreed. \n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Not Agreed. \n");
+    } else
+    {
+        printf("INVALID");
+    }
+}
