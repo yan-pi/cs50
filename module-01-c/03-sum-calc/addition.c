3e183a13aa42b66de08a4bb5c5bfe1e3a2e239e1
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-04-12 01:53:22 +0000

    add description

--- module-01-c/03-sum-calc/addition.c
+++ module-01-c/03-sum-calc/addition.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
-    printf("%i\n", x + y);
+    //sum long numbers, use int to sum short numbers but has datatype bug for int
+    long x = get_long("x: ");
+    long y = get_long("y: ");
+    printf("%li\n", x + y);
 }
