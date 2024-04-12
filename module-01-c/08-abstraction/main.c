9d0f020c73f1f56bedaaea6949b8b6cb19577571
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-04-12 12:49:48 +0000

    refactor: refactor to handle asbtraction and scope

--- module-01-c/08-abstraction/main.c
+++ module-01-c/08-abstraction/main.c
@@ -1,26 +1,34 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void meow(void); //prtototype
-int repeat(void);
+void repeat_string(string word, int times); // prototype
+int get_positive(void); // prototype
 
 int main(void)
 {
-    int c = repeat();
-    
-    for (size_t i = 0; i < c; i++)
+    string word = get_string("Digite a palavra que você quer repetir:\n");
+    int repetitions = get_positive(); // Pega o número de repetições
+
+    repeat_string(word, repetitions); // Chama a função para repetir a palavra
+}
+
+void repeat_string(string word, int times)
+{
+    for (int i = 0; i < times; i++)
     {
-        meow();
+        printf("%s\n", word);
     }
 }
 
-void meow(void)
+int get_positive(void)
 {
-    printf("meow\n");
-}
+    int num;
 
-int repeat(void)
-{
-    int c = get_int("how many times will repeat?\n");
-    return c;
+    do
+    {
+        num = get_int("Quantas vezes deseja repetir a palavra?\n");
+    } 
+    while (num < 1);
+    
+    return num;
 }
