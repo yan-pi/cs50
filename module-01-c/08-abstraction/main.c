723592e61a9371e8c7b4970050a12634a94518e5
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-04-12 11:46:24 +0000

    add abstraction

---
+++ module-01-c/08-abstraction/main.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void meow(void)
+{
+    printf("meow\n");
+}
+
+int repeat(void)
+{
+    int c = get_int("how many times will repeat?\n");
+    return c;
+}
+
+int main(void)
+{
+    int c = repeat();
+    
+    for (size_t i = 0; i < c; i++)
+    {
+        meow();
+    }
+}
